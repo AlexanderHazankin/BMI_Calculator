728505b2afb9943967b35b39c626e3c208411cd5
Alexander Hazankin <103335228+AlexanderHazankin@users.noreply.github.com>
2022-11-20 07:46:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,3 @@ elif result > 40.0:
     print("You are obese (Class III)")
 else:
     print("ValueError")
-
-print(""
-      "NOTE:\n"
-      "*BMI used to predict an individual's health,\n"
-      " rather than as a statistical measurement for groups,\n"
-      " the BMI has limitations that can make it less useful than some of the alternatives,\n"
-      " especially when applied to individuals with abdominal obesity,\n"
-      " short stature, or unusually high muscle mass.")
-print("*BMIs under 20 and over 25 have been associated with higher all-cause mortality,\n"
-      " with the risk increasing with distance from the 20–25 range.")
